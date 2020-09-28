7ba3f594baea7b72119007a9b011c08d9fd5271d
itsamankrsingh <itsamankrsingh@gmail.com>
2020-09-28 21:07:06 +0530

    initial commit

---
+++ app/src/main/java/com/magician/myapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.magician.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.magician.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
+        viewModel.editTextContent.observe(this, Observer {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        })
+    }
+}
---
+++ app/src/main/java/com/magician/myapplication/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.magician.myapplication
+
+import androidx.databinding.Bindable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    val currentRandomFruitName: LiveData<String>
+        get() = FakeRepo.currentRandomFruitName
+
+    fun onChangeRandomFruitClick() = FakeRepo.changeCurrentRandomFruitName()
+
+
+    val editTextContent = MutableLiveData<String>()
+
+    private val _displayedEditTextContent = MutableLiveData<String>()
+    val displayedEditTextContent: LiveData<String>
+        get() = _displayedEditTextContent
+
+    fun onDisplayEditTextContentClick() {
+        _displayedEditTextContent.value = editTextContent.value
+    }
+
+    fun onSelectRandomEditTextFruit(){
+        editTextContent.value=FakeRepo.getRandomFruitName()
+    }
+}
